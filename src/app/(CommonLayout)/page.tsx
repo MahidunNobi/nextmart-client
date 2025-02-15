7a1f4ce409d5ffacccc98d2beb53622d20e2d001
MahidunNobi <mdmahidunnobi@gmail.com>
2025-02-15 16:42:53 +0600

    Getting Current user data using cookies

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,6 +1,10 @@
+import { getCurrentUser } from "@/services/AuthServices";
 import React from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  console.log(user);
+
   return <div>Wellcome to next mart</div>;
 };
 
--- src/services/AuthServices/index.tsx
+++ src/services/AuthServices/index.tsx
@@ -1,3 +1,6 @@
+"use server";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
 
 export const registerUser = async (userData: FieldValues) => {
@@ -26,10 +29,26 @@ export const loginUser = async (userData: FieldValues) => {
       },
       body: JSON.stringify(userData),
     });
+    const result = await res.json();
+    if (result.success) {
+      (await cookies()).set("accessToken", result.data.accessToken);
+    }
 
-    return res.json();
+    return result;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = (await cookies()).get("accessToken")?.value;
+  let decodedData = null;
+
+  if (accessToken) {
+    decodedData = await jwtDecode(accessToken);
+    return decodedData;
+  } else {
+    return null;
   }
 };
