3472e4ab1f9f07c7228a5535830f6d6778c5db41
MahidunNobi <mdmahidunnobi@gmail.com>
2025-02-25 17:50:49 +0600

    Creating: new categories

--- src/components/modules/shop/Category/CreateCategoryModel.tsx
+++ src/components/modules/shop/Category/CreateCategoryModel.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import NMImageUploader from "@/components/ui/core/NMImageUploader";
+import ImagePreviewer from "@/components/ui/core/NMImageUploader/ImagePreviewer";
 import {
   Dialog,
   DialogContent,
@@ -16,9 +18,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { createCategory } from "@/services/Category";
+import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 const CreateCategoryModel = () => {
+  const [imageFiles, setImageFiles] = useState<File[] | []>([]);
+  const [imagePreview, setImagePreview] = useState<string[] | []>([]);
   const form = useForm();
 
   const {
@@ -28,6 +36,17 @@ const CreateCategoryModel = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     try {
       console.log(data);
+      const formData = new FormData();
+      formData.append("data", JSON.stringify(data));
+      formData.append("icon", JSON.stringify(imageFiles[0]));
+
+      const res = await createCategory(formData);
+      console.log(res);
+      if (res.success) {
+        toast.success(res.message);
+      } else {
+        toast.error(res.message);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -40,55 +59,68 @@ const CreateCategoryModel = () => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Add Category</DialogTitle>
         </DialogHeader>
         {/* ------------- Actual Form----------- */}
         <div>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
-              {/* ------------Email------- */}
               <FormField
                 control={form.control}
-                name="email"
+                name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Email</FormLabel>
+                    <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input
-                        type="email"
-                        {...field}
-                        value={field.value || ""}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* ------------ Password --------- */}
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        {...field}
-                        value={field.value || ""}
-                      />
+                      <Input type="name" {...field} value={field.value || ""} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
 
+              <div className="mt-4 flex flex-col md:flex-row items-center justify-between gap-3">
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          className="h-36 md:w-72"
+                          {...field}
+                          value={field.value || ""}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {imagePreview.length > 0 ? (
+                  <ImagePreviewer
+                    setImageFiles={setImageFiles}
+                    imagePreview={imagePreview}
+                    setImagePreview={setImagePreview}
+                    className="mt-8"
+                  />
+                ) : (
+                  <div className="mt-8">
+                    <NMImageUploader
+                      setImageFiles={setImageFiles}
+                      setImagePreview={setImagePreview}
+                      label="Upload Logo"
+                    />
+                  </div>
+                )}
+              </div>
+
               <Button
                 // disabled={!reCaptachaStatus}
                 type="submit"
                 className="mt-5 w-full"
               >
-                {isSubmitting ? "Logging..." : "Login"}
+                {isSubmitting ? "Creating..." : "Create"}
               </Button>
             </form>
           </Form>
